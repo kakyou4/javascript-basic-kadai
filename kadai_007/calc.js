ba95829f54c378171e999cde48e7c7d6c5c93c54
kakyou4 <kakyou4@infoseek.jp>
2024-10-29 18:38:54 +0900

    課題ファイルを追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+// 変数の定義
+let num = Math.floor(Math.random() * 99) + 1;
+
+// コンソールへ出力（確認用）
+console.log(num);
+
+// 条件式
+if ((num % 3 ) === 0) {
+  console.log('3の倍数です');
+} 
+else if ((num % 5) === 0) {
+  console.log('5の倍数です');
+}
+else if ((num % 15) === 0) {
+  console.log('15の倍数です');
+}
+else {
+  console.log(num);
+}
