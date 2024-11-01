1c8b92cd2a3c629c379f294fe50775270ec9182a
kakyou4 <kakyou4@infoseek.jp>
2024-11-01 19:23:21 +0900

    課題ファイルを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth()+1,
+  date.getDate(),
+  date.getFullYear(),
+];
+
+console.log(year + '年' + month + '月' + day + '日');
+
+// getFullYear()
+// getMonth()
+// getDate()
