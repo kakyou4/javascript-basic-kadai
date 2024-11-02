0aaf728cbe485ee8150b5eb5065003d53143fafe
kakyou4 <kakyou4@infoseek.jp>
2024-11-02 18:49:12 +0900

    Update square.js

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,7 +1,7 @@
 // 引数で渡した値を二乗した計算結果を戻り値として返す関数を定義する
-const Square = (num) => {
+const square = (num) => {
   return num * num;
 }
 
 // 関数の戻り値を出力する
-console.log(Square(10));
+console.log(square(10));
